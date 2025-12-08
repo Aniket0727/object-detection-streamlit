9ec873df08abbf4c8bc23a83260afdbdf068215b
Aniket Kangude <142888993+Aniket0727@users.noreply.github.com>
2025-12-08 14:38:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 
 
 # Loading trained model
-MODEL_PATH = "https://drive.google.com/uc?export=download&id=1Gi8VusaxD7LtZuJxMuXUzXUnY4qH70M8"
+MODEL_PATH = "https://github.com/yourusername/yourrepo/releases/download/v1.0/model.h5"
 CLASS_NAMES_PATH = "https://drive.google.com/uc?export=download&id=13lIg55uwkMDChgG_gTrrpU-xmuOhntDs"
 
 
@@ -54,5 +54,6 @@ if uploaded_file is not None:
     label, confidence = predict_image(image)
 
     st.success(f" Detected: **{label}** ({confidence:.2f}% confidence)")
+
 
 
